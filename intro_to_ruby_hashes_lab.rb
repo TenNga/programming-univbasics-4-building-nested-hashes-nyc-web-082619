ffc50b96b3be7ee3b840d20cac04b6bc5b154250
Kevin Karma <kevinkarma55@gmail.com>
2019-08-23 17:46:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,6 +4,7 @@ def base_hash
 	hash_without_keyword = {:railroads => {} }
 end
 
+
 def monopoly_with_second_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
